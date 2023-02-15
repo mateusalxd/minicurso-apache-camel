c88f3eaff638fd846a3815b851152a0802a45297
mateusalxd <mateusalxd@gmail.com>
2023-02-15 13:38:07 -0300

    video-004 - Depurando uma rota com um processor

--- integracaoarquivo/src/main/java/com/github/mateusalxd/minicursoapachecamel/integracaoarquivo/IntegracaoArquivo.java
+++ integracaoarquivo/src/main/java/com/github/mateusalxd/minicursoapachecamel/integracaoarquivo/IntegracaoArquivo.java
@@ -9,6 +9,9 @@ public class IntegracaoArquivo extends RouteBuilder {
     public void configure() throws Exception {
         from("file:{{diretorioEntrada}}?delay=5000")
                 .routeId("integracao-arquivo")
+                .process(exchange -> {
+                    System.out.println(exchange.getMessage().getBody(String.class));
+                })
                 .log("Processando o arquivo: ${file:name}")
                 // .setHeader(FileConstants.FILE_NAME, simple("${date:now:HHmmss}_${file:name}"))
                 .to("file:{{diretorioSaida}}?fileName=${date:now:HHmmss}_${file:name}");
