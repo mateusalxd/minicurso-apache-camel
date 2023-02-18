385051d7592b033f7c28c40429ca28604e16f72d
mateusalxd <mateusalxd@gmail.com>
2023-02-18 14:13:59 -0300

    video-005 - Choice, Throttle e o componente HTTP

--- integracaoarquivo/src/main/java/com/github/mateusalxd/minicursoapachecamel/integracaoarquivo/IntegracaoArquivo.java
+++ integracaoarquivo/src/main/java/com/github/mateusalxd/minicursoapachecamel/integracaoarquivo/IntegracaoArquivo.java
@@ -1,19 +1,32 @@
 package com.github.mateusalxd.minicursoapachecamel.integracaoarquivo;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.file.FileConstants;
+import org.apache.camel.component.http.HttpConstants;
+import org.apache.camel.support.builder.Namespaces;
 
 public class IntegracaoArquivo extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        var ns = new Namespaces("ns", "http://www.portalfiscal.inf.br/nfe");
+
         from("file:{{diretorioEntrada}}?delay=5000")
                 .routeId("integracao-arquivo")
-                .process(exchange -> {
-                    System.out.println(exchange.getMessage().getBody(String.class));
-                })
                 .log("Processando o arquivo: ${file:name}")
-                // .setHeader(FileConstants.FILE_NAME, simple("${date:now:HHmmss}_${file:name}"))
-                .to("file:{{diretorioSaida}}?fileName=${date:now:HHmmss}_${file:name}");
+                .setProperty("CNPJ", xpath("{{xpathCnpjTransportadora}}", ns))
+                .choice()
+                .when(exchangeProperty("CNPJ").isEqualTo("1"))
+                .to("file:{{diretorioTransportadora1}}?fileName=${date:now:HHmmss}_${file:name}")
+                .when(exchangeProperty("CNPJ").isEqualTo("2"))
+                    .throttle(1).timePeriodMillis(5000).asyncDelayed()
+                        .setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
+                        .setHeader(HttpConstants.HTTP_URI, constant("{{urlApiTransportadora2}}"))
+                        .setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
+                        .setHeader(HttpConstants.CONTENT_TYPE).constant("application/xml")
+                        .to("http:servidorTransportadora2")
+                        .endChoice()
+                .otherwise()
+                .log("Transportadora não integrada")
+                .end();
     }
 }
